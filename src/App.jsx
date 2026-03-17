b238b89f590a6f92259fffe46e98a453c2aa4bef
CarlosVazPorto <carlosvazporto@gmail.com>
2026-03-17 15:42:22 -0300

    feat(App.jsx): aprimora estilização CSS no  styled component FundoGradiente

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,10 @@ import styled from "styled-components";
 
 const FundoGradiente = styled.div`
 
-/* 1440 - Desktop */
-
-position: relative;
-width: 1440px;
-height: 2655px;
-
 /* Gradiente fundo */
 background: linear-gradient(174.61deg, #041833 4.16%, #04244F 48%, #154580 96.76%);
-mix-blend-mode: normal;
+width: 100%;
+min-height: 100vh;
 
 `;
 
