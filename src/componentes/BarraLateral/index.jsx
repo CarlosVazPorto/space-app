39ab316ef601101525151563921942d6d7aa26fa
CarlosVazPorto <carlosvazporto@gmail.com>
2026-03-22 16:25:53 -0300

    feat(BarraLateral/index.jsx): estiliza a ul

--- src/componentes/BarraLateral/index.jsx
+++ src/componentes/BarraLateral/index.jsx
@@ -1,16 +1,43 @@
+import styled from "styled-components";
 
+const ListaEstilizada = styled.ul`
+    list-style: none;
+    padding: 0;
+    margin: 0;
+    width: 212px;
+`;
 
 const BarraLateral = () => {
     return (
         <aside>
             <nav>
-                <ul>
+                <ListaEstilizada>
                     <li>
                         <a href="">
                             Início
                         </a> 
                     </li>
-                </ul>
+                    <li>
+                        <a href="">
+                            Mais visitas
+                        </a> 
+                    </li>
+                    <li>
+                        <a href="">
+                            Mais curtidas
+                        </a> 
+                    </li>
+                    <li>
+                        <a href="">
+                            Novas
+                        </a> 
+                    </li>
+                    <li>
+                        <a href="">
+                            Surpreenda-me
+                        </a> 
+                    </li>
+                </ListaEstilizada>
             </nav>
         </aside>
     );
