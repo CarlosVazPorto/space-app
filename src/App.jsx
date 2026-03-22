dcc65f8f2497a509f8e2ed3eec0bde3b90f519ad
CarlosVazPorto <carlosvazporto@gmail.com>
2026-03-22 16:08:25 -0300

    feat(App.jsx): importa o componente BarraLateral

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import styled from "styled-components";
 import EstilosGlobais from "./componentes/EstilosGlobais";
 import Cabecalho from "./componentes/Cabecalho";
+import BarraLateral from "./componentes/BarraLateral";
 
 const FundoGradiente = styled.div`
 
@@ -17,6 +18,7 @@ function App() {
     <FundoGradiente>
       <EstilosGlobais />
       <Cabecalho />
+      <BarraLateral />
     </FundoGradiente>
   );
 };
