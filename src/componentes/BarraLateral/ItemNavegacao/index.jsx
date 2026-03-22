6c9e0ade6aa265a977225ae9ab835df690bb7d39
CarlosVazPorto <carlosvazporto@gmail.com>
2026-03-22 17:01:36 -0300

    feat(BarraLateral/ItemNavegacao/index.jsx): acrescenta atributo alt à imagem do ícone

--- src/componentes/BarraLateral/ItemNavegacao/index.jsx
+++ src/componentes/BarraLateral/ItemNavegacao/index.jsx
@@ -1,13 +1,13 @@
 import styled from "styled-components";
 
 const ItemNavegacaoEstilizado = styled.li`
-    
+
 `;
 
 const ItemNavegacao = ({ children, iconeAtivo, iconeInativo, ativo = false }) => {
     return (
         <ItemNavegacaoEstilizado>
-            <img src={ativo ? iconeAtivo : iconeInativo} alt="" />
+            <img src={ativo ? iconeAtivo : iconeInativo} alt="Ícone do link para a página" />
             {children}
         </ItemNavegacaoEstilizado>
     );
