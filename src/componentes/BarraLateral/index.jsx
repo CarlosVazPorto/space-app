bf0baad0949bcde0750bedf2949a65556a1e88a1
CarlosVazPorto <carlosvazporto@gmail.com>
2026-03-22 16:07:19 -0300

    feat(BarraLateral/index.jsx): cria estrutura inicial da aside

---
+++ src/componentes/BarraLateral/index.jsx
@@ -0,0 +1,19 @@
+
+
+const BarraLateral = () => {
+    return (
+        <aside>
+            <nav>
+                <ul>
+                    <li>
+                        <a href="">
+                            Início
+                        </a> 
+                    </li>
+                </ul>
+            </nav>
+        </aside>
+    );
+};
+
+export default BarraLateral;
