0afbcc9e785248d072f3295fe4c6578ba954442a
CarlosVazPorto <carlosvazporto@gmail.com>
2026-03-22 16:48:22 -0300

    feat(BarraLateral/index.jsx): adiciona os componentes ItemNavegacao

--- src/componentes/BarraLateral/index.jsx
+++ src/componentes/BarraLateral/index.jsx
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import ItemNavegacao from "./ItemNavegacao";
 
 const ListaEstilizada = styled.ul`
     list-style: none;
@@ -12,7 +13,41 @@ const BarraLateral = () => {
         <aside>
             <nav>
                 <ListaEstilizada>
-
+                    <ItemNavegacao
+                        iconeAtivo={}
+                        iconeInativo={}
+                        ativo={true}
+                    >
+                        Início
+                    </ItemNavegacao>
+                    <ItemNavegacao
+                        iconeAtivo={}
+                        iconeInativo={}
+                        ativo
+                    >
+                        Mais vistas
+                    </ItemNavegacao>
+                    <ItemNavegacao
+                        iconeAtivo={}
+                        iconeInativo={}
+                        ativo
+                    >
+                        Mais curtidas
+                    </ItemNavegacao>
+                    <ItemNavegacao
+                        iconeAtivo={}
+                        iconeInativo={}
+                        ativo
+                    >
+                        Novas
+                    </ItemNavegacao>
+                    <ItemNavegacao
+                        iconeAtivo={}
+                        iconeInativo={}
+                        ativo
+                    >
+                        Surpeenda-me
+                    </ItemNavegacao>
                 </ListaEstilizada>
             </nav>
         </aside>
