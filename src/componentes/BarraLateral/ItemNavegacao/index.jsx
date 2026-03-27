34d7b5f25aa2202656d0645b5fc88df5040254b7
CarlosVazPorto <carlosvazporto@gmail.com>
2026-03-27 19:18:03 -0300

    feat(BarraLateral/ItemNavegacao/index.jsx): aprimora estilos do ItemNavegacaoEstilizado

--- src/componentes/BarraLateral/ItemNavegacao/index.jsx
+++ src/componentes/BarraLateral/ItemNavegacao/index.jsx
@@ -6,6 +6,9 @@ const ItemNavegacaoEstilizado = styled.li`
     margin-bottom: 30px;
     cursor: pointer;
     color: ${ props => props.$ativo ? '#7B78E5' : '#D9D9D9' };
+    display: flex;
+    align-items: center;
+    gap: 22px;
 `;
 
 const ItemNavegacao = ({ children, iconeAtivo, iconeInativo, ativo = false }) => {
