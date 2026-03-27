536d2aef7d4d2ce859dca6af0a884da586f135dc
CarlosVazPorto <carlosvazporto@gmail.com>
2026-03-27 19:03:50 -0300

    feat(BarraLateral/ItemNavegacao/index.jsx): estiliza o ItemNavegacaoEstilizado

--- src/componentes/BarraLateral/ItemNavegacao/index.jsx
+++ src/componentes/BarraLateral/ItemNavegacao/index.jsx
@@ -1,12 +1,16 @@
 import styled from "styled-components";
 
 const ItemNavegacaoEstilizado = styled.li`
-
+    font-size: 24px;
+    line-height: 29px;
+    margin-bottom: 30px;
+    cursor: pointer;
+    color: #7B78E5;
 `;
 
 const ItemNavegacao = ({ children, iconeAtivo, iconeInativo, ativo = false }) => {
     return (
-        <ItemNavegacaoEstilizado $props={{ ativo }}>
+        <ItemNavegacaoEstilizado $ativo={{ ativo }}>
             <img src={ativo ? iconeAtivo : iconeInativo} alt="Ícone do link para a página" />
             {children}
         </ItemNavegacaoEstilizado>
