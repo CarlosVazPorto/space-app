87a6745bb0312737065c65a3189242a054addeca
CarlosVazPorto <carlosvazporto@gmail.com>
2026-04-08 16:50:50 -0300

    feat(Galeria/index.jsx): estiliza GaleriaContainer e SecaoFluida

--- src/componentes/Galeria/index.jsx
+++ src/componentes/Galeria/index.jsx
@@ -1,5 +1,15 @@
 import Titulo from "../Titulo";
 import Tags from "./Tags";
+import Populares from "./Populares";
+import { styled } from "styled-components";
+
+const GaleriaContainer = styled.div`
+    display: flex;
+`;
+
+const SecaoFluida = styled.section`
+    flex-grow: 1;
+`;
 
 const Galeria = () => {
     return (
