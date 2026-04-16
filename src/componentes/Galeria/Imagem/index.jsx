38e910c1755e1cd5b9b00e82d3c8128d9a9a203c
CarlosVazPorto <carlosvazporto@gmail.com>
2026-04-16 18:07:26 -0300

    style(Imagem/index.jsx): estiliza o <figure>

--- src/componentes/Galeria/Imagem/index.jsx
+++ src/componentes/Galeria/Imagem/index.jsx
@@ -1,3 +1,38 @@
+import styled from "styled-components";
+
+
+const Figure = styled.figure`
+    width: ${(props) => (props.$expandida ? '90%' : '460px')};
+    max-width: 100%;
+    margin: 0;
+    display: flex;
+    flex-direction: column;
+
+    & > img {
+        max-width: 100%;
+        border-radius: 20px 20px 0 0;
+    }
+
+    figcaption {
+        background-color: #001634;
+        border-radius: 0px 0px 20px 20px;
+        color: #FFFFFF;
+        box-sizing: border-box;
+        padding: 12px;
+        h3 {
+            font-family: 'GandhiSansBold';
+        }
+        h4 {
+            flex-grow: 1;
+        }
+        h3,
+        h4 {
+            margin: 0;
+            font-size: 16px;
+        }
+    }
+`;
+
 const Imagem = ({ foto }) => {
     return (
         <figure>
@@ -5,7 +40,7 @@ const Imagem = ({ foto }) => {
             <figcaption>
                 <h3>{foto.titulo}</h3>
                 <footer>
-                    <p>Fonte</p>
+                    <h4>Fonte</h4>
                     <button>Favorito</button>
                     <button>Expandir</button>
                 </footer>
