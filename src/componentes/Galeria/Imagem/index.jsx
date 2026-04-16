32b838257f97831699cdfbb37d68584b1fc935d9
CarlosVazPorto <carlosvazporto@gmail.com>
2026-04-16 17:44:14 -0300

    feat(Imagem/index.jsx): estrutura inicial do componente

--- src/componentes/Galeria/Imagem/index.jsx
+++ src/componentes/Galeria/Imagem/index.jsx
@@ -1,5 +1,17 @@
-const Imagem = () => {
-    return ();
+const Imagem = ({ foto }) => {
+    return (
+        <figure>
+            <img src={foto.url} alt={foto.titulo} />
+            <figcaption>
+                <h3>{foto.titulo}</h3>
+                <footer>
+                    <p>Fonte</p>
+                    <button>Favorito</button>
+                    <button>Expandir</button>
+                </footer>
+            </figcaption>
+        </figure>
+    );
 };
 
 export default Imagem;
