702a7714555aa673e8a32e89e152b7a95861713f
CarlosVazPorto <carlosvazporto@gmail.com>
2026-04-23 15:59:21 -0300

    feat(componentes/ModalZoom/index.jsx): cria estrutura inicial do componente ModalZoom

---
+++ src/componentes/ModalZoom/index.jsx
@@ -0,0 +1,14 @@
+const ModalZoom = () => {
+    return (
+        <>
+            <dialog open>
+                <p>Greetings, one and all!</p>
+                <form method="dialog">
+                    <button>Ok</button>
+                </form>
+            </dialog>
+        </>
+    );
+};
+
+export default ModalZoom;
