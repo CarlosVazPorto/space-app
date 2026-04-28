2e4e613e8479d828bf1536afb596ed5896001078
CarlosVazPorto <carlosvazporto@gmail.com>
2026-04-28 16:34:52 -0300

    feat(Galeria/Populares/index.jsx): estiliza a ColunaFotos

--- src/componentes/Galeria/Populares/index.jsx
+++ src/componentes/Galeria/Populares/index.jsx
@@ -1,4 +1,12 @@
+import { styled } from "styled-components";
 import Titulo from "../../Titulo";
+import fotos from "../../Titulo";
+
+const ColunaFotos = styled.section`
+    display: flex;
+    flex-direction: column;
+    gap: 16px;
+`;
 
 const Populares = () => {
     return (
