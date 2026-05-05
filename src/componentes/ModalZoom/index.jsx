90b74ee7e0b0b2abd4ee34ca1079898311e1df64
CarlosVazPorto <carlosvazporto@gmail.com>
2026-05-05 16:46:20 -0300

    feat(ModalZoom/index.jsx): recebe função aoAlternarFavorito como props da Imagem

--- src/componentes/ModalZoom/index.jsx
+++ src/componentes/ModalZoom/index.jsx
@@ -29,7 +29,7 @@ const DialogEstilizado = styled.dialog`
     }
 `;
 
-const ModalZoom = ({ foto, aoFechar }) => {
+const ModalZoom = ({ foto, aoFechar, aoAlternarFavorito }) => {
     return (
         <>
             {foto && 
@@ -39,6 +39,7 @@ const ModalZoom = ({ foto, aoFechar }) => {
                         <Imagem 
                             foto={foto} 
                             expandida={true} 
+                            aoAlternarFavorito={aoAlternarFavorito}
                         />
                         <form method="dialog">
                             <BotaoIcone formMethod="dialog">
