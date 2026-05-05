13dafc3d4d2029b02f6254ad56c423eca0fbc619
CarlosVazPorto <carlosvazporto@gmail.com>
2026-05-05 16:55:58 -0300

    feat(App.jsx): atualiza estado de favorita para a FotoSelecionada

--- src/App.jsx
+++ src/App.jsx
@@ -41,6 +41,13 @@ const App = () => {
   const [fotoSelecionada, setFotoSelecionada] = useState(null);
 
   const aoAlternarFavorito = (foto) => {
+    if (foto.id === fotoSelecionada?.id) {
+      setFotoSelecionada({
+        ...fotoSelecionada,
+        favorita: !fotoSelecionada.favorita
+      });
+    };
+
     setFotosDaGaleria(fotosDaGaleria.map((fotoDaGaleria) => {
       return {
         ...fotoDaGaleria,
